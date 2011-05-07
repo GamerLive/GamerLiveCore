da186d9f5731505f780dacef55851666e2dba16e
tobmaps <spambot42@yandex.ru>
2011-05-07 18:10:11 +0700

    More cleanups

--- sql/Wintergrasp/OutdoorPvPWG_characters.sql
+++ sql/Wintergrasp/OutdoorPvPWG_characters.sql
@@ -1,5 +1,5 @@
 /*WorldStates Characters DB*/
-DELETE FROM worldstates WHERE entry IN (31001,31002,31003,31004);
+DELETE FROM `worldstates` WHERE `entry` IN (31001,31002,31003,31004);
 INSERT INTO `worldstates` VALUES
 ('31001', '0', 'WG war status'),
 ('31002', '0', 'WG clock'),
--- src/server/scripts/Northrend/VaultOfArchavon/instance_vault_of_archavon.cpp
+++ src/server/scripts/Northrend/VaultOfArchavon/instance_vault_of_archavon.cpp
@@ -51,11 +51,9 @@ class instance_archavon : public InstanceMapScript
                 {
                     if (OutdoorPvPWG *pvpWG = (OutdoorPvPWG*)sOutdoorPvPMgr->GetOutdoorPvPToZoneId(4197))
                     {
-                       if ((pvpWG->getDefenderTeam()==TEAM_ALLIANCE) && (m_player->ToPlayer()->GetTeam() == ALLIANCE))
-                           return;
-                       else if ((pvpWG->getDefenderTeam()!=TEAM_ALLIANCE) && (m_player->ToPlayer()->GetTeam() == HORDE))
-                           return;
-                       else m_player->CastSpell(m_player, SPELL_TELEPORT_FORTRESS, true);
+                       bool alliance = pvpWG->getDefenderTeam() == TEAM_ALLIANCE;
+                       if ((alliance && m_player->GetTeam() == HORDE) || (!alliance && m_player->GetTeam() == ALLIANCE))
+                           m_player->CastSpell(m_player, SPELL_TELEPORT_FORTRESS, true);
                     }
                 }
             }
